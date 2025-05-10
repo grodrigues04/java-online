8e5bbf9c36488a8c68792a4bd3850e84a7889e9a
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2025-05-09 21:52:31 -0300

    feat: routes initial configs

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,16 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// const adminRoutes = require('./routes/usuario')
+const usuario = require('./routes/usuario')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/", (req, res)=>{
-    console.log(req.body)
-    res.send("ola")
-})
-
-
+app.use(usuario)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- BackEnd/routes/usuario.js
+++ BackEnd/routes/usuario.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+// guadmin
 
 router.get("/teste", (req, res)=>{
+    console.log(req.body)
     res.send("Funcionou")
 })
+
+module.exports = router
